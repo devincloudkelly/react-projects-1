ed0b5087a45ca88622baea0ae763021252ef7070
devincloudkelly <devin.cloud.kelly@gmail.com>
2021-06-08 13:20:59 -0500

    Add Menu to overall project

--- 05-menu/setup/src/App.js
+++ 05-menu/setup/src/App.js
@@ -1,10 +1,37 @@
-import React, { useState } from 'react';
-import Menu from './Menu';
-import Categories from './Categories';
-import items from './data';
+import React, { useState } from "react";
+import Menu from "./Menu";
+import Categories from "./Categories";
+import items from "./data";
+
+// this uses the Set data structure to remove duplicates and only return unique values. It returns an object so we spread it into an array with the 'all' category
+const allCategories = ["all", ...new Set(items.map((item) => item.category))];
 
 function App() {
-  return <h2>menu project setup</h2>;
+  const [menuItems, setMenuItems] = useState(items);
+  // these categories are set up so the categories can be dynamic and accept future additions/changes to the categories, auto generating menus and buttons.
+  const [categories, setCategories] = useState(allCategories);
+
+  const updateMenu = (menu) => {
+    if (menu === "all") {
+      setMenuItems(items);
+    } else {
+      const newItems = items.filter((item) => item.category === menu);
+      setMenuItems(newItems);
+    }
+  };
+
+  return (
+    <main>
+      <section className="menu section">
+        <div className="title">
+          <h2>our menu</h2>
+          <div className="underline"></div>
+        </div>
+        <Categories updateMenu={updateMenu} categories={categories} />
+        <Menu items={menuItems} />
+      </section>
+    </main>
+  );
 }
 
 export default App;
--- 05-menu/setup/src/Categories.js
+++ 05-menu/setup/src/Categories.js
@@ -1,7 +1,26 @@
-import React from 'react';
+import React from "react";
 
-const Categories = () => {
-  return <h2>categories component</h2>;
+const Categories = ({ updateMenu, categories }) => {
+  // don't need an eventHandler since we passed down 'updateMenu()' and can just pass in the category
+  // const sendMenu = (e) => {
+  //   updateMenu(e.target.innerText.toLowerCase());
+  // };
+
+  return (
+    <section className="btn-container">
+      {categories.map((category, index) => {
+        return (
+          <button
+            className="filter-btn"
+            onClick={() => updateMenu(category)}
+            key={index}
+          >
+            {category}
+          </button>
+        );
+      })}
+    </section>
+  );
 };
 
 export default Categories;
