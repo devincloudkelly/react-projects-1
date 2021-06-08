a1162a33e4100de370406399265010be417a644e
devincloudkelly <devin.cloud.kelly@gmail.com>
2021-06-08 13:57:06 -0500

    Add Lorem Ipsum to overall project

--- 08-lorem-ipsum/setup/src/App.js
+++ 08-lorem-ipsum/setup/src/App.js
@@ -1,11 +1,56 @@
-import React, { useState } from 'react';
-import data from './data';
+import React, { useState } from "react";
+import data from "./data";
+
 function App() {
-  
-  
+  const [lorem, setLorem] = useState([]);
+  const [paragraphs, setParagraphs] = useState(0);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (paragraphs <= 0) {
+      setLorem(data.slice(0, 1));
+      return;
+    }
+    if (paragraphs > data.length) {
+      setLorem(data);
+      return;
+    }
+    const newArray = data.slice(0, paragraphs);
+    console.log(newArray);
+    setLorem(newArray);
+    return;
+  };
+
+  const generateLorem = () => {
+    if (lorem.length === 0) {
+      return;
+    }
+    return lorem.map((paragraph, index) => {
+      return <p key={index}>{paragraph}</p>;
+    });
+  };
+
   return (
-  <h2>lorem ipsum project setup</h2>
-    )
+    <section className="section-center">
+      <h3>tired of boring lorem ipsum?</h3>
+      <form className="lorem-form" onSubmit={handleSubmit}>
+        <label htmlFor="paragraphs">paragraphs: </label>
+        <input
+          type="number"
+          id="paragraphs"
+          name="paragraphs"
+          value={paragraphs}
+          onChange={(e) => {
+            setParagraphs(e.target.value);
+          }}
+        />
+        <button className="btn" type="submit">
+          generate
+        </button>
+      </form>
+      <article className="lorem-text">{generateLorem()}</article>
+    </section>
+  );
 }
 
 export default App;
