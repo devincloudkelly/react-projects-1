9fa00f1f81e52dba66a593f24f825fc3d2f83f5d
devincloudkelly <devin.cloud.kelly@gmail.com>
2021-06-08 13:01:21 -0500

    Add tours to overall project

--- 02-tours/setup/src/App.js
+++ 02-tours/setup/src/App.js
@@ -1,11 +1,65 @@
-import React, { useState, useEffect } from 'react'
-import Loading from './Loading'
-import Tours from './Tours'
+import React, { useState, useEffect } from "react";
+import Loading from "./Loading";
+import Tours from "./Tours";
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
-const url = 'https://course-api.com/react-tours-project'
+const url = "https://course-api.com/react-tours-project";
 function App() {
-  return <h2>Tours Project Setup</h2>
+  const [isLoading, setIsLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+
+  const removeTour = (id) => {
+    const newTours = tours.filter((tour) => tour.id !== id);
+    setTours(newTours);
+    console.log(newTours);
+  };
+
+  // need to fetch the tours and pass in to the Tours component.
+
+  const fetchTours = async () => {
+    setIsLoading(true);
+
+    try {
+      const response = await fetch(url);
+      const tours = await response.json();
+      setTours(tours);
+      setIsLoading(false);
+    } catch (error) {
+      setIsLoading(false);
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchTours();
+  }, []);
+
+  if (isLoading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  }
+  if (tours.length === 0) {
+    return (
+      <main>
+        <div className="title">
+          <h2>no tours left</h2>
+          <button className="btn" onClick={fetchTours}>
+            refresh
+          </button>
+        </div>
+      </main>
+    );
+  }
+  if (tours.length > 0) {
+    return (
+      <main>
+        <Tours tours={tours} removeTour={removeTour} />
+      </main>
+    );
+  }
 }
 
-export default App
+export default App;
--- 02-tours/setup/src/Tours.js
+++ 02-tours/setup/src/Tours.js
@@ -1,7 +1,24 @@
-import React from 'react';
-import Tour from './Tour';
-const Tours = () => {
-  return <h2>tours component</h2>;
+import React from "react";
+import Tour from "./Tour";
+const Tours = ({ tours, removeTour }) => {
+  const renderTours = () => {
+    return tours.map((tour) => {
+      return <Tour {...tour} key={tour.id} removeTour={removeTour} />;
+    });
+  };
+
+  return (
+    <>
+      <section>
+        <div className="title">
+          {/* need to set up conditional render for 'our tours' or if none, 'no tours left' and refresh button */}
+          <h2>our tours</h2>
+          <div className="underline"></div>
+        </div>
+        <div>{renderTours()}</div>
+      </section>
+    </>
+  );
 };
 
 export default Tours;
