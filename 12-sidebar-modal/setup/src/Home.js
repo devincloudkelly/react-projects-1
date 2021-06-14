c8c68a2916b3f31372a61814be7cb634bb85ddc6
devincloudkelly <devin.cloud.kelly@gmail.com>
2021-06-14 17:08:25 -0500

    Add Sidebar to overall project

--- 12-sidebar-modal/setup/src/Home.js
+++ 12-sidebar-modal/setup/src/Home.js
@@ -1,8 +1,25 @@
-import React, { useContext } from 'react'
-import { FaBars } from 'react-icons/fa'
+import React, { useContext } from "react";
+import { FaBars } from "react-icons/fa";
+import { AppContext, useGlobalContext } from "./context";
 
 const Home = () => {
-  return <h2>home component</h2>
-}
+  // this is one way of accessing Context from the context.js file. Requires two imports, one of 'useContext' from 'react', and one of the context component we created, 'AppContext' from our context file.
+  // const data = useContext(AppContext);
+  // console.log(data);
 
-export default Home
+  // second method of accessing our context is by creating a custom hook in the context file, then exporting it and importing it where needed. If we do that, we only need to import that hook, we don't need to import useContext from react or the specific context component we created - we already have access to those through the custom hook.
+  const { openSidebar, openModal } = useGlobalContext();
+
+  return (
+    <main>
+      <button className="sidebar-toggle" onClick={openSidebar}>
+        <FaBars />
+      </button>
+      <button className="btn" onClick={openModal}>
+        show modal
+      </button>
+    </main>
+  );
+};
+
+export default Home;
